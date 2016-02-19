f1bb9c30acc39b5239e932db4489aa79646c6935
maximesab <maximesabran@gmail.com>
2016-02-19 09:07:13 +0100

    Login updated to POST and CALL request

--- app/src/main/java/com/example/max/methis/LoginActivity.java
+++ app/src/main/java/com/example/max/methis/LoginActivity.java
@@ -1,38 +1,49 @@
 package com.example.max.methis;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import 	java.net.URLEncoder;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
 import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -41,20 +52,14 @@ import static android.Manifest.permission.READ_CONTACTS;
  * Modified by: Maxime Sabran
  * Removed : auto completion login, auto-registering
  * */
-public class LoginActivity extends AppCompatActivity  {
+public class LoginActivity extends AppCompatActivity   {
 
     /**
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
+    public String mResponse;
 
-    /**
-     *  authentication store containing known user names and passwords
-     *  changed the user/password as required in the assessment
-     */
-    private static final String[] USER_CREDENTIALS = new String[]{
-            "cytrus:cytrus"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -108,79 +113,30 @@ public class LoginActivity extends AppCompatActivity  {
         if (mAuthTask != null) {
             return;
         }
+        String url = "http://demo.u.cytrus.biz/login.php";
 
         // Reset errors.
         mUserView.setError(null);
         mPasswordView.setError(null);
+        mResponse="";
 
         // Store values at the time of the login attempt.
         String user = mUserView.getText().toString();
         String password = mPasswordView.getText().toString();
+        mAuthTask = new UserLoginTask(user, password,url);
 
-        boolean cancel = true;
-        View focusView = null;
+        //Allow AsyncTasks to be made in parallel
 
-        // Check for a valid (length<4) password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_incorrect_password));
-            focusView = mPasswordView;
-
-        }
-
-        // Check for a valid user.
-        if (TextUtils.isEmpty(user)) {
-            mUserView.setError(getString(R.string.error_field_required));
-            focusView = mUserView;
-
-        } else if (!isuserValid(user)) {
-            mUserView.setError(getString(R.string.error_invalid_username));
-            focusView = mUserView;
-            cancel = true;
-        }
-        /*
-         *added a password check.
-         */
-        for (String credential : USER_CREDENTIALS) {
-            String[] pieces = credential.split(":");
-
-            if (pieces[0].equals(user) && pieces[1].equals(password) ) {
-                // Account exists, return true if the password matches.
-                mUserView.setError(getString(R.string.login_success));
-                focusView = mUserView;
-                cancel=false;
-            }
-        }
-
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            mPasswordView.setError(getString(R.string.error_incorrect_password));
-            focusView = mPasswordView;
-
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-
-            showProgress(true);
-            mAuthTask = new UserLoginTask(user, password);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            mAuthTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, (Void[]) null);
+        }else {
             mAuthTask.execute((Void) null);
-            /*
-             *linked the completion of the loggin activity to the Master/Flow activity
-             */
-            Context context = mUserView.getContext();
-            Intent intent = new Intent(context, CarModelListActivity.class);
-            context.startActivity(intent);
-
         }
-    }
 
-    private boolean isuserValid(String user) {
-        return true;
-    }
+        mAuthTask = null;
+        showProgress(false);
 
-    private boolean isPasswordValid(String password) {
-        return password.length() > 4;
+
     }
 
     /**
@@ -223,44 +179,96 @@ public class LoginActivity extends AppCompatActivity  {
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mUser;
+        private URL mURL;
         private final String mPassword;
+        /*
+         * Constructor
+         * display a failed message if the url was malformed
+         */
 
-        UserLoginTask(String user, String password) {
+        UserLoginTask(String user, String password, String url) {
             mUser = user;
             mPassword = password;
+
+            try{
+                mURL = new URL(url);
+            }catch (Exception MalformedURLException){
+                mResponse ="failed: wrong url";
+            }
         }
+
+        /*
+         * POST and CALL request to the url in order to login
+         */
 
         @Override
         protected Boolean doInBackground(Void... params) {
 
+
+            System.setProperty("http.keepAlive", "false");
+            OutputStreamWriter writer = null;
+            BufferedReader reader = null;
+            URLConnection connexion = null;
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
+                // Encoding parameters for the POST
+                String datas = "username"+"="+mUser + "&"+"password"+"="+mPassword;
 
-            for (String credential : USER_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mUser)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+
+                // Opening the connection to the url and allowing an upstream input
+
+                connexion = mURL.openConnection();
+                connexion.setDoOutput(true);
+
+
+                // Sending the data
+                writer = new OutputStreamWriter(connexion.getOutputStream());
+
+                // Writing the data
+                writer.write(datas);
+
+                // Emptying the stream
+                writer.flush();
+
+                // Making the CALL request and reading it
+                reader = new BufferedReader(new InputStreamReader(connexion.getInputStream()));
+                String ligne;
+                while ((ligne = reader.readLine()) != null) {
+                    mResponse+=ligne;
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try{writer.close();}catch(Exception e){}
+                try{reader.close();}catch(Exception e){}
             }
-
 
             return true;
         }
 
+        /*
+         * the AsyncTask ie is done:
+         * Checking the CALL result
+         * if success: starting the master/flow activity
+         * else: display the error message
+         */
         @Override
         protected void onPostExecute(final Boolean success) {
             mAuthTask = null;
-            showProgress(false);
 
+            if (mResponse.contains("success")) {
+                showProgress(true);
+                Context context = mUserView.getContext();
+                Intent intent = new Intent(context, CarModelListActivity.class);
+                context.startActivity(intent);
+            }if(mResponse.contains("failed")){
+                mPasswordView.setError(mResponse.substring(mResponse.indexOf(":") + 1));
+                mPasswordView.requestFocus();
+                mAuthTask = null;
+                showProgress(false);
+            }
             if (success) {
-                finish();
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.setError(mResponse.substring(mResponse.indexOf(":") + 1));
                 mPasswordView.requestFocus();
             }
         }
